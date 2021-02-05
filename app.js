78125df30f8a77106e1eca3e363f70cf3b36b9d6
VictorPlagelat <v.plagelat@gmail.com>
2021-02-05 13:28:30 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.querySelector(".burger").addEventListener("click", function () {  
+    document.querySelector("header nav div .link").classList.toggle("visible");
+    });
+
+document.querySelector(".burger").addEventListener("click", function () {  
+    document.querySelector("header nav div .burger").classList.toggle("cross");
+    }); 
+
